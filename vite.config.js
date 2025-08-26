c79fe45faa36efe279d78229976077ff99538cb9
Micael Rocha <micaelrrocha13@gmail.com>
2025-08-25 23:35:28 -0300

    refactor: remove unnecessary comments and allowedHosts configuration from Vite server settings

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		host: true, // permite acesso externo
-		port: 5173, // você pode ajustar a porta se quiser
-		allowedHosts: [
-			'all'
-		]
+		host: true,
+		port: 5173
 	}
 });
